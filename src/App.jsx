1eb572a02d22df99f9d1d9c3fa5107a58b42a173
Alex Hyde <alexhyde@Alexs-MacBook-Pro.local>
2023-12-07 15:03:35 -0500

    Dynamic Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
+import MovieList from "./Components/MovieList/MovieList";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList />
     </div>
   );
 }
